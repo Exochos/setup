f9cd7cb0f2dcf2583cb4f372bd98c09df103d60a
Jeremy Ward <exochos@gmail.com>
2025-01-16 16:33:46 -0800

    Create nginx.sh

---
+++ nginx.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# 04_install_basics.sh
+# Installs Nginx, zip, etc.
+
+set -euo pipefail
+source "$(dirname "$0")/env.sh"
+
+echo "=== 04_install_basics: Installing packages..." | tee -a "$LOG_FILE"
+
+{
+  apt install -y nginx zip
+} 2>&1 | tee -a "$LOG_FILE"
+
+echo "=== 04_install_basics: Done." | tee -a "$LOG_FILE"
