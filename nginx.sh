b36a4124141df991d08a988981ac954a83739813
Jeremy Ward <exochos@gmail.com>
2025-01-16 16:40:38 -0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-#
-# 04_install_basics.sh
-# Installs Nginx, zip, etc.
+# nginx.sh --> Install Nginx, Configure 
 
 set -euo pipefail
 source "$(dirname "$0")/env.sh"
@@ -9,7 +7,7 @@ source "$(dirname "$0")/env.sh"
 echo "=== 04_install_basics: Installing packages..." | tee -a "$LOG_FILE"
 
 {
-  apt install -y nginx zip
+  apt-get install -y nginx
 } 2>&1 | tee -a "$LOG_FILE"
 
 echo "=== 04_install_basics: Done." | tee -a "$LOG_FILE"
